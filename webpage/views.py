2dacb88bd622f9a47ea598eba46e50427e162a98
tpiyxpxrn <pyppw55@gmail.com>
2024-07-03 11:09:39 +0700

    add new web2

--- webpage/views.py
+++ webpage/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+def indexPage(request):
+        return render(request, 'index.html')
+def aboutUs(request):
+        return render(request, 'about.html')
+def contact(request):
+        return render(request, 'contact.html')
